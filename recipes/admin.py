610d09505e0eaeb0fc8158404610eab13a15340e
guilherme <guilherme@email.com>
2022-06-16 13:06:15 -0300

    Criação dos model e admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'is_published', 'created_at',
+                    'updated_at']
+    list_display_links = ('id', 'title',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at',
+                    'updated_at']
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.models import User
 from django.db import models
+from stdimage import StdImageField
 
-# Create your models here.
+
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(Base):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(Base):
+    preparation_choices = [
+        ('Min', 'Minutos'),
+        ('Hr', 'Horas'),
+    ]
+    servings_choices = [
+        ('Uni', 'Unidades'),
+        ('Por', 'Porções'),
+        ('Pes', 'Pessoas'),
+    ]
+    title = models.CharField(max_length=65)
+    preparation_time = models.IntegerField()
+    preparation_choices = models.CharField(
+        max_length=12, choices=preparation_choices, default='')
+    servings = models.IntegerField()
+    servings_choices = models.CharField(
+        max_length=12, choices=servings_choices, default='')
+    ingredients = models.TextField()
+    preparation_method = models.TextField()
+    is_published = models.BooleanField(default=False, blank=True)
+    image = StdImageField(upload_to='path/to/img',
+                          variations={'variation_image':
+                                      {'width': 370, 'height': 217}})
+
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = ('Recipe')
+        verbose_name_plural = ('Recipes')
